21c23b3f0515a7d670f07fdab81201efec23f68e
Seow Yung Hoe <seowyunghoe@hotmail.com>
2017-03-17 20:24:31 +0800

    no message

---
+++ RoketClone/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  RoketClone
+//
+//  Created by Seow Yung Hoe on 17/03/2017.
+//  Copyright © 2017 Seow Yung Hoe. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ RoketClone/RegisterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterViewController.swift
+//  RoketClone
+//
+//  Created by Seow Yung Hoe on 17/03/2017.
+//  Copyright © 2017 Seow Yung Hoe. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
